aeef4724412f395dc9afd27687943fe16da9c0b1
Juliano Tavares <juliano.tavaress@gmail.com>
2021-02-11 20:11:17 -0300

    added main style

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -1,6 +1,16 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: min-content(601px, 100%);
+
+  @media (min-width: 500px) {
+    border-left: 1px solid var(--gray);
+    border-right: 1px solid var(--gray);
+  }
+`;
 
 export const Header = styled.div``;
 
